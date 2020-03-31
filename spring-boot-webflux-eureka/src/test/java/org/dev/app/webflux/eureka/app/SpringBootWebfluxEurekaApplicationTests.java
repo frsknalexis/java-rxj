2982853ace504424344c21d4bea13eaaedb8adc2
Alexis Gutierrez <alexisgutierrezf.1997@gmail.com>
2020-03-31 02:13:49 -0500

    [agregando eureka server para trabajar con microservicios]

---
+++ spring-boot-webflux-eureka/src/test/java/org/dev/app/webflux/eureka/app/SpringBootWebfluxEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.app.webflux.eureka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
